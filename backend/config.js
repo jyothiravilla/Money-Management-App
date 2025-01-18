280851812b3436d0bd661c5b949f1c9ee0f2419b
Nagajyothi Ravilla <162462220+jyothiravilla@users.noreply.github.com>
2025-01-17 20:35:00 -0500

    Delete backend directory

--- backend/config.js
+++
@@ -1,9 +0,0 @@
-// backend/config.js
-module.exports = {
-    db: {
-        host: 'localhost',
-        user: 'root',
-        password: 'nagaj2020!', // Replace 'your_password' with the actual password
-        database: 'money_management'  // Replace 'money_management' with the actual database name
-    }
-};
