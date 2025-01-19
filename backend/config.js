01adc2a225a9e3a6d6cfc51c01bd6ad7f8f38cd7
Nagajyothi Ravilla <162462220+jyothiravilla@users.noreply.github.com>
2025-01-19 12:49:19 -0500

    Add files via upload

---
+++ backend/config.js
@@ -0,0 +1,9 @@
+// backend/config.js
+module.exports = {
+    db: {
+        host: 'localhost',
+        user: 'root',
+        password: 'nagaj2020!', // Replace 'your_password' with the actual password
+        database: 'money_management'  // Replace 'money_management' with the actual database name
+    }
+};
